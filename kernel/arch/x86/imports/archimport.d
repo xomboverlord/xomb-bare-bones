04c33b63c3f115e033fd3e6d09f8cfc147cf816f
wilkie <wilkie05@gmail.com>
2009-03-23 08:26:37 +0800

    Added architecture import selection to show how this would be done for LDC and DSSS. This is important (as documentation for these is light) but no actual work for the targets are done.
    The archimport.d file is for importing the specific architecture and its interfaces. It can be imported by any module, as I will do for the next commit.
    
    Signed-off-by: The XOmB Overlord <overlord@xomb.net>

---
+++ kernel/arch/x86/imports/archimport.d
@@ -0,0 +1,12 @@
+/*
+ * archimport.d
+ *
+ * This file will publically import all modules used to define
+ * the architecture interfaces provided by this architecture.
+ *
+ */
+
+module archimport;
+
+// Example:
+// import kernel.arch.x86.main;
